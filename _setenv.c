413baa4eeb93470cb667d43bbc387ecc99a0f90c
KRLoyd <135@holbertonschool.com>
2017-04-11 18:13:57 +0000

    restart _setenv.c

--- _setenv.c
+++ _setenv.c
@@ -11,22 +11,43 @@ int _setenv(const char *name, const char *value, int overwrite)
 {
 
 	extern char **environ;
-	struct list_env *envptr;
-	unsigned int i;
-	char *env_name, env_value, single_env, last_node;
+	int i, compare;
+	unsigned int nlen, vlen;
+	char *new_array;
 
-	i = 0;
+	if (name == NULL)
+		return (-1);
 
-	while (environ[i] != NULL)
+	nlen = _strlen_rec((char *)name);
+	    if (nlen == 0)
+		    return (-1);
+
+	vlen = _strlen_rec((char *)value);
+	new_array = malloc(sizeof(nlen + vlen + 2));
+	if (new_array == NULL)
 	{
-		single_env = environ[i];
-		env_name = strtok(single_env, "=");
-		env_value = strtok(NULL, " ");
-		last_node = _env_add_node(env_name, env_value);
-		if (add_result = NULL)
-			return (-1);
-		i++;
+		perror("malloc for new_array failed");
+		return (-1);
 	}
 
+	new_array = _strcpy(new_array, (char *)name);
+	new_array = _strcat(new_array, "=");
+	new_array = _strcat(new_array, (char *)value);
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		compare = strncmp(name, environ[i], nlen);
+		if (compare == 0 && overwrite == 0)
+		{
+			printf("environ[i] before: %s\n", environ[i]);
+			environ[i] = new_array;
+			printf("\tenviron[i] after: %s\n", environ[i]);
+		}
+		else if (compare == 0 && overwrite != 0)
+		{
+			printf("environ[i]: %s\n", environ[i]);
+		}
+
+	}
 	return (0);
 }
