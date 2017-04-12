96bdc669b3e6b982a08d14caa35b3ccca79d9757
KRLoyd <135@holbertonschool.com>
2017-04-12 22:17:09 +0000

    add code blocks/rework

--- _which.c
+++ _which.c
@@ -1,29 +1,60 @@
 #include "test_shell.h"
 /**
- * main - looks for files in the current PATH
+ * _which - looks for files in the current PATH
  *
- * Return: 0 (SUCCESS), -1 (FAILURE)
+ * Return: filepath (SUCCESS), NULL (FAILURE)
  */
 
-int main(int ac, __attribute__((unused))char **av)
+char *_which(char *str, list_t *list)
 {
-	list_t path;
-/**	char *variable;
-	char *buffer;
-	char *token;
-	size_t size;**/
+	char *half_path;
+	char *full_path;
+	char *liststr;
+	int access_result;
 
-	if (ac < 2)
+	liststr = malloc(sizeof(BUFSIZE));
+	if (liststr == NULL)
 	{
-		printf("Usage: _which filename ...\n");
-		return (-1);
+		perror("Malloc Error:\n");
+		return (NULL);
 	}
-/**	path = _link_path("PATH");
-	if (path != 0)
+	half_path = malloc(sizeof(BUFSIZE));
+	if (half_path == NULL)
 	{
-		printf("Error linking path\n");
-		return (-1);
-		}**/
-	printf("path.str: %s, path.len: %d\n", path.str, path.len);
-	return (0);
+		perror("Malloc Error:\n");
+		return (NULL);
+	}
+	full_path = malloc(sizeof(BUFSIZE));
+	if (full_path == NULL)
+	{
+		perror("Malloc Error:\n");
+	}
+	while (list != NULL)
+	{
+		clear_buff(full_path);
+		clear_buff(half_path);
+		clear_buff(liststr);
+
+		liststr = _strcpy(liststr, list->str);
+		half_path = _strcat(liststr, "/");
+		if (half_path == NULL)
+			return (NULL);
+		printf("half-path: %s\n", half_path);
+		full_path = _strcat(half_path, str);
+		printf("full-path: %s\n\n", full_path);
+		if (full_path == NULL)
+			return (NULL);
+
+		else
+		{
+			access_result = access(full_path, X_OK);
+			if (access_result == 0)
+			{
+				printf("can access file!\n");
+				return (full_path);
+			}
+		}
+		list = list->next;
+	}
+	return (str);
 }
