9e30d2345857a14876f2f77e08d94ad40195eca6
KRLoyd <135@holbertonschool.com>
2017-04-12 22:20:56 +0000

    add _which prototype

--- test_shell.h
+++ test_shell.h
@@ -11,7 +11,6 @@
 #define BUFSIZE 1024
 #define ENV extern char **environ
 #define PROMPT "$ "
-#define BUFSIZE 1024
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -36,15 +35,17 @@ typedef struct list_env
 	char *value;
 	struct list_env *next;
 } list_env;
-list_t *_add_node_end(list_t *head, char *str);
+list_t *_add_node_end(list_t **head, char *str);
+void clear_buff(char *buffer);
 list_env *_env_add_node(list_env *head, char *name, char *value);
 void error_handle(int num, int fd);
 char *_getenv(const char *name);
-list_t _link_path(const char *str);
+list_t *_link_path(void);
 size_t _print_liststr(list_t *h);
 int _setenv(const char *name, const char *value, int overwrite);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strncmp(char *s1, char *s2, unsigned int n);
 int _strlen_rec(char *s);
+char *_which(char *str, list_t *list);
 #endif /* __TEST_SHELL__ */
