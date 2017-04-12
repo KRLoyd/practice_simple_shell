6d16d2595f920a0f2697f289886a2200eb1157b2
KRLoyd <135@holbertonschool.com>
2017-04-12 22:16:04 +0000

    add and remove printf statements

--- _strcat.c
+++ _strcat.c
@@ -12,10 +12,17 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	len = _strlen_rec(dest);
+/**	printf("BEFORE:\n\tdest: %s\n\tsrc: %s\n", dest, src);**/
+
 
 	for (i = 0, j = len; src[i] != '\0'; i++, j++)
 	{
 		dest[j] = src[i];
 	}
+	if (dest[j] != '\0')
+	{
+		dest[j] = '\0';
+	}
+/**	printf("AFTER:\n\tdest: %s\n\n", dest); **/
 	return ((char *)dest);
 }
