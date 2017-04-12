9596d9192f9908ad0cf9d1890b40b2232bd918bd
KRLoyd <135@holbertonschool.com>
2017-04-12 22:14:27 +0000

    remove printf stmnts

--- _link_path.c
+++ _link_path.c
@@ -10,26 +10,37 @@ list_t *_link_path(void)
 	char *dir;
 	list_t *head;
 	list_t *add_result;
+	size_t nodes;
 
 	head = NULL;
 	to_link = _getenv("PATH");
 	if (to_link == NULL)
+	{
+		printf("Error 1");
 		return (NULL);
-
+	}
 
 	dir = strtok(to_link, ":");
 
 	while (to_link != NULL)
 	{
+		printf("dir: \n");
 		if (dir == NULL)
 		{
 			break;
 		}
-		add_result = _add_node_end(head, dir);
-		if (add_result == NULL)
-			return (NULL);
+		if (dir != NULL)
+		{
+			add_result =  _add_node_end(&head, dir);
+			if (add_result == NULL)
+			{
+				printf("Error 1");
+				return (NULL);
+				}
+		}
 		dir = strtok(NULL, ":");
 	}
-
+	nodes =	_print_liststr(head);
+	printf("Nodes: %lu\n", nodes);
 	return (head);
 }
